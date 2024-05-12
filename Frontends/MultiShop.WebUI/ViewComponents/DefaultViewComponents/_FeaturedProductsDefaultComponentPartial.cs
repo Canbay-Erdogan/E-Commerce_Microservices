04fbf27c366ec75f7a2a8988122c51a5adc9a92a
canbay-erdogan <erdogan.canbay@hotmail.com>
2024-05-12 04:16:52 +0300

    UserPageViewLayout

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeaturedProductsDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeaturedProductsDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
